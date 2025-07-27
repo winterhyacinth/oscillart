9574dfceb554948de7fcbbac0762bc46642f3245
rome <145407644+winterhyacinth@users.noreply.github.com>
2025-07-27 03:52:28 +0000

    Allowed notes to be used as well as frequencies to request audio

--- main.js
+++ main.js
@@ -3,6 +3,17 @@ const input = document.getElementById('input');
 
 const audioCtx = new AudioContext();
 
+notenames = new Map();
+notenames.set("A", 493.9);
+notenames.set("B", 440);
+notenames.set("C", 261.6);
+notenames.set("D", 293.7);
+notenames.set("E", 329.6);
+notenames.set("F", 349.2);
+notenames.set("G", 392.0);
+var usernotes = String(input.value);
+frequency(notenames.get(usernotes)); 
+
 // define the frequency function
 function frequency(pitch) {
     // create Oscillator and Gain nodes inside the function
@@ -23,7 +34,10 @@ function frequency(pitch) {
 }
 
 function handle() {
+    var usernotes = String(input.value);
+    frequency(notenames.get(usernotes)); 
    frequency(input.value);
-   audioCtx.resume();
-gainNode.gain.value = 0;
+   audioCtx.resume();   
+    gainNode.gain.value = 0;
+    
 }
