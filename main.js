1f3d980ee801d11a0b4b6d834258f477b6151d49
rome <145407644+winterhyacinth@users.noreply.github.com>
2025-07-27 02:37:27 +0000

    Made button play frequency sounds

--- main.js
+++ main.js
@@ -1,31 +1,29 @@
+
 const input = document.getElementById('input');
 
-// create web audio api elements
 const audioCtx = new AudioContext();
-const gainNode = audioCtx.createGain();
 
-
-// create Oscillator node
-const oscillator = audioCtx.createOscillator();
-oscillator.connect(gainNode);
-gainNode.connect(audioCtx.destination);
-oscillator.type = "sine";
-
-
-gainNode.gain.setValueAtTime(100, audioCtx.currentTime);
-oscillator.frequency.setValueAtTime(pitch, audioCtx.currentTime);
-gainNode.gain.setValueAtTime(0, audioCtx.currentTime + 1);
-
-oscillator.start();
-gainNode.gain.value = 0;
-
+// define the frequency function
 function frequency(pitch) {
+    // create Oscillator and Gain nodes inside the function
+    const oscillator = audioCtx.createOscillator();
+    const gainNode = audioCtx.createGain();
 
+    oscillator.type = "sine";
+    oscillator.frequency.setValueAtTime(pitch, audioCtx.currentTime);
+
+    gainNode.gain.setValueAtTime(100, audioCtx.currentTime);
+    gainNode.gain.setValueAtTime(0, audioCtx.currentTime + 1);
+
+    oscillator.connect(gainNode);
+    gainNode.connect(audioCtx.destination);
+
+    oscillator.start();
+    oscillator.stop(audioCtx.currentTime + 1);
 }
 
 function handle() {
    frequency(input.value);
    audioCtx.resume();
 gainNode.gain.value = 0;
-
 }
