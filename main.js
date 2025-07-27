1cf62c1e64c22cfdd3f045ca8b9c751ac6edf53c
rome <145407644+winterhyacinth@users.noreply.github.com>
2025-07-27 05:08:07 +0000

    Created Drawing Canvas for waves

--- main.js
+++ main.js
@@ -3,6 +3,20 @@ const input = document.getElementById('input');
 
 const audioCtx = new AudioContext();
 
+
+
+//define canvas variables
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d"); 
+var width = ctx.canvas.width;
+var height = ctx.canvas.height;
+
+
+var interval = null;
+var amplitude = 40;
+
+
+
 notenames = new Map();
 notenames.set("A", 493.9);
 notenames.set("B", 440);
@@ -16,6 +30,8 @@ frequency(notenames.get(usernotes));
 
 // define the frequency function
 function frequency(pitch) {
+    freq = pitch / 10000;
+
     // create Oscillator and Gain nodes inside the function
     const oscillator = audioCtx.createOscillator();
     const gainNode = audioCtx.createGain();
@@ -36,8 +52,37 @@ function frequency(pitch) {
 function handle() {
     var usernotes = String(input.value);
     frequency(notenames.get(usernotes)); 
-   frequency(input.value);
+   drawWave();
    audioCtx.resume();   
-    gainNode.gain.value = 0;
+    gainNode.gain.value = 0.5;
+}
+
+
+var counter = 0;
+function drawWave() {
+	ctx.clearRect(0, 0, width, height);
+    x = 0;
+    y = height/2;
+    ctx.moveTo(x, y);
+    ctx.beginPath(); 
     
+    counter = 0;
+    interval = setInterval(line, 20);
+
+}
+
+function line() {
+    
+    y = height/2 + (amplitude * Math.sin(2(Math.PI * freq * x) ) )
+    ctx.lineTo(x,y);
+    ctx.stroke();
+    x = x + 1;
+
+   //increase counter by 1 to show how long interval has been run
+   counter++;
+
+   if(counter > 50) {
+       clearInterval(interval);
+  }
+
 }
